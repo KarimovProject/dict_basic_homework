0c234166956417c88d898d4bddb8e3398e8f9492
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-14 09:12:21 -0400

    6-misol

--- oldest_person.py
+++ oldest_person.py
@@ -6,4 +6,14 @@ def oldest(people:dict):
     Returns:
         str: the name of the oldest person
     """
-    return
+    #print(oldest_student({"Bernita Ahner": 12, "Kristie Marsico": 11, 
+    #                 "Sara Pardee": 14, "Fallon Fabiano": 11, 
+     #                 "Nidia Dominique": 15})) 
+    #return max(people, key=people.get)
+    student1 = {"name" : "Abdullatif",  "age" : 19}
+    student2 = {"name" : "Maqsud", "age" : 16}
+    student3 = {"name" : "Xurshid", "age" : 17}
+
+    student_age = [student1["age"],student2["age"], student3["age"]]
+    student_age = sorted(student_age)
+    return student_age[2:3]
